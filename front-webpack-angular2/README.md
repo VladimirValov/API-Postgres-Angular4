b58658ef1454d27b8ed4fb8ceac3b3e498dd03c1
Vladimir Valov <vvalov@smedialink.com>
2017-07-18 15:41:17 +0300

    webpack build + http service

---
+++ front-webpack-angular2/README.md
@@ -0,0 +1,9 @@
+cd front-webpack-angular2
+npm install
+
+# Develop watch
+npm run build:watch
+
+
+# Production
+npm run build
---
+++ front-webpack-angular2/src/app/services/mock-report.ts
@@ -0,0 +1,24 @@
+export const DATA: any =  {
+    count: 255,
+    data: { 
+        society: 2,
+        lonely: 2,
+
+        drink: 3,
+        hash: 3,
+
+        feel: 4,
+        relation: 4,
+
+        economy: 5,
+
+        community: 6,
+        contribute: 6,
+
+        smoke: 7,
+        snus: 7,
+
+        sex: 8,
+        deny: 8
+    }
+}
